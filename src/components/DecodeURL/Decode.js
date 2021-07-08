164761ae0608f58e16beb56f1c03ee4e9815c7ed
Sumit Jadiya <sumit.jadiya@appdirect.com>
2021-07-08 13:50:29 +0530

    refactor decode method

--- src/components/DecodeURL/Decode.js
+++ src/components/DecodeURL/Decode.js
@@ -1,17 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import instance from '../../config/axios';
 
 function Decode() {
     const { code } = useParams();
     const history = useHistory();
-    const [url, setUrl] = useState('');
 
     useEffect(() => {
         instance.get(`/${code}`)
             .then(res => {
                 let finalData = res.data.decodedString;
-                setUrl(finalData)
                 console.log("final ->", finalData)
                 if (finalData)
                     window.location.replace(finalData);
@@ -19,7 +17,6 @@ function Decode() {
             .catch(error => {
                 return history.push("/");
             })
-
     }, []);
 
     return <div></div>;
